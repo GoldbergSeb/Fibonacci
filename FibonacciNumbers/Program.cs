f38cf97fc48e66aed78d95d0a850628bb1f3a698
Sebastian Goldberger <Sebastian.Goldberger@students.bs-lin2.ac.at>
2021-12-22 08:29:50 +0100

    Update output again

--- FibonacciNumbers/Program.cs
+++ FibonacciNumbers/Program.cs
@@ -15,7 +15,7 @@ namespace FibonacciNumbers {
             }
             else
             {
-                Console.WriteLine("Die wievielte Fibonacci-Zahl soll berechnet werden?");
+                Console.WriteLine("Die wievielte Fibonacci-Nummer soll berechnet werden?");
                 input = Console.ReadLine();
                 int number = int.Parse(input);
                 int result = Fib(number);
